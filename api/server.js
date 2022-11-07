139f97b413513711983248ddb5cf44d2da8c3040
Sailesh Kafle <saileshkafle47@gmail.com>
2022-11-07 20:55:50 +0545

    server ready

--- api/server.js
+++ api/server.js
@@ -1,4 +1,4 @@
-const express = require('express.js');
+const express = require('express');
 const mongoose = require('mongoose');
 const cors=require('cors');
 
@@ -6,3 +6,17 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+mongoose.connect("mongodb://127.0.0.1:27017/mern-todoApp",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(()=> console.log("Connected to DB"))
+.catch(console.err);
+
+app.listen(3001, ()=> console.log("Server started on port 3001"));
+
+
+
+
+
+
